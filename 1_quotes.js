2dbc8e0ff683b0ef61a4328b64406a5619f18bf5
CCandruss <63433057+CCandruss@users.noreply.github.com>
2020-05-08 10:59:05 -0500

    Random Quote Generator Submission
    This is the final draft for the purposes of the project although it may be edited to add additional features if inspiration strikes.

--- 1_quotes.js
+++ 1_quotes.js
@@ -1,50 +1,75 @@
+//array of quotes available for selection
 let quotes = [
   {
     quote: `Our capacity to draw happiness from aesthetic objects or material 
     goods in fact seems critically dependent on our first satisfying a more important 
     range of emotional or psychological needs, among them the need for understanding, 
     for love, expression and respect.` ,
-    author: "Alain De Botton",
-    citation: 'keepinspiring.me',
-    year: 2002 
+    source: "Alain De Botton",
   },  
   {
     quote: `When a man is denied the right to live the life he believes in, he has no 
     choice but to become an outlaw.`,
-    author: 'Nelson Mandela',
+    source: 'Nelson Mandela',
     citation: `Mandela's Way`,
     year: 2009 
   }, 
   {
     quote: `I learned that courage was not the absence of fear, but the triumph over it. 
     The brave man is not he who does not feel afraid, but he who conquers that fear.`,
-    author: 'Nelson Mandela',
-    citation: `Mandela's Way`,
-    year: 2009
+    source: 'Nelson Mandela',
   },
   {
     quote: `As people spin faster and faster in the pursuit of merely personal happiness, 
     they become exhausted in the futile effort of chasing themselves.`,
-    author: 'Andrew Delbanco',
-    citation: 'keepinspiring.me',
-    year: 2000
+    source: 'Andrew Delbanco',
   },
   {
     quote: `To enjoy good health, to bring true happiness to one's family, to bring peace 
     to all, one must first learn to discipline and control one's own mind. if a man can 
     control his mind he can find the key to Enlightenment, and all wisdom and virtue will 
     naturally come to him.`,
-    author: 'Buddha' ,
-    citation: 'Dharma',
-    year: "543 BC"
+    source: 'Buddha' 
+  },
+  {
+    quote: 'There is no education like adversity.',
+    source: 'Benjamin Disraeli'
   }
 ];
 let number;
-
-function randomNum(){
-  number = Math.floor(Math.random()*5);
-  return number;
+let selectedQuote;
+//function gathering a random number to select a quote object from array
+function getRandomQuote(){
+  function randomNum () {
+    number = Math.floor(Math.random()*quotes.length);
+    return number;
+  }
+  selectedQuote = quotes[randomNum()];
+  return selectedQuote;
+}
+//print function identifying what values are present in the object and adding those values to the HTML
+function printQuote (message){
+  let obj = getRandomQuote();
+  let output = '';
+  if(obj.quote){
+    output += '<p class = "quote" >' + obj.quote + '</p>';
+  }
+  if(obj.source){
+    output += '<p class = "source" >' + obj.source + '</p>';
+  }
+  if(obj.citation){
+    output += '<p class = "citation" >' + obj.citation + '</p>';
+  } 
+  if(obj.year){
+    output += '<p class= "year">' + obj.year + '</p>';
+  }
+  document.getElementById('quote-box').innerHTML = output;
 }
 
-
-console.log(quotes[randomNum()]);
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//timer to initiate quote change
+var intervalID = window.setInterval(printQuote, 10000);
